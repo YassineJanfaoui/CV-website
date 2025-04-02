b50df995e553ac6fa07c09b32e527b6e2ba0478c
YassineJanfaoui <janfaouimohamed16@gmail.com>
2025-04-02 22:36:07 +0100

    added new project and css changes for projects section

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function toggleMenu() {
-  const menu = document.querySelector('.menu-links');
-  const icon = document.querySelector('.hamburger-icon');
-  menu.classList.toggle('open');
-  icon.classList.toggle('open');
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
+function revealSections() {
+  const sections = document.querySelectorAll("section");
+
+  sections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (sectionTop < windowHeight - 100) {
+      section.classList.add("show");
+    }
+  });
+}
+
+// Run when scrolling
+window.addEventListener("scroll", revealSections);
+
+// Run once on page load in case some sections are already in view
+window.addEventListener("load", revealSections);
